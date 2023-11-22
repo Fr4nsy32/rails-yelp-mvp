a38e8273e1ce58cdd1fb19f0e05ed56c121e1e7b
Andi Sina <andy.sina32@gmail.com>
2023-11-22 16:11:34 +0100

    Yelp working

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,32 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require "faker"
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+Review.destroy_all
+
+puts "Creating restaurants..."
+7.times do
+  category = %w(chinese italian japanese french belgian)
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name ,
+    address: Faker::Address.city,
+    phone_number: Faker::PhoneNumber.cell_phone ,
+    category: category.sample
+  )
+  puts "Created #{restaurant.name}"
+  restaurant.save!
+  3.times do
+    rating = [0, 1, 2, 3, 4, 5]
+    review = Review.new(
+      content: Faker::Lorem.paragraph,
+      rating: rating.sample,
+      restaurant_id: restaurant.id
+    )
+    puts "Created review"
+    review.save!
+  end
+end
+puts "Finished!"
